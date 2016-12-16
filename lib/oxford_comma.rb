4dc4984bace66bd36d97b4e09e3d1bf4e2f1107c
Ahmed ALFLAHY <lfight522@gmail.com>
2016-12-16 16:25:14 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
+if array.size > 2
+postion = array.pop
+array.push("and #{postion}")
+array.join(", ")
 
+elsif array.size == 2
+  postion = array.pop
+array.push(" and #{postion}")
+array.join
+elsif array.size == 1
+ array.join
+end
 end
